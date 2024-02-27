b808152649fd7c8cbbb18499530de0bebd6dfb8b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-02-27 18:00:12 +0530

    fix: FFmpeg libplacebo depend

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -17,6 +17,7 @@ ExternalProject_Add(ffmpeg
         libvpl
         libjxl
         mbedtls
+        libplacebo
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_TAG ea3d24bbe3c58b171e55fe2151fc7ffaca3ab3d2
